756425f0a7e7f8ad22f0548281fba064d73b9e23
Joseph Davies <joseph.davies@ext.ec.eu>
2020-02-18 09:41:31 +0100

    added pointData, mapPadding and backgroundColor options. 3035 now transformed using a d3 linear scale

--- examples/node/index.js
+++ examples/node/index.js
@@ -1,12 +1,22 @@
 import { reglMapAnimation } from "../../index.js";
+const d3 = require("d3");
+
 const containerDiv = document.getElementById("container");
 
-reglMapAnimation({
-  csvURL: "../../pop_5km.csv",
-  container: containerDiv,
-  pointWidth: 1,
-  pointMargin: 1,
-  delayAtEnd: 1,
-  colors: ["#005cff", "#55e238", "#ebff0a", "#ffce08", "#ff0f00", "#a6306f"],
-  stops: [0, 100, 1000, 5000, 10000, 30000]
+d3.csv("../../pop_5km.csv", d => {
+  return {
+    value: d.value,
+    y: +d.y,
+    x: +d.x
+  };
+}).then(pointData => {
+  reglMapAnimation({
+    pointData,
+    container: containerDiv,
+    pointWidth: 1,
+    pointMargin: 1,
+    delayAtEnd: 1,
+    colors: ["#005cff", "#55e238", "#ebff0a", "#ffce08", "#ff0f00", "#a6306f"],
+    stops: [0, 100, 1000, 5000, 10000, 30000]
+  });
 });
